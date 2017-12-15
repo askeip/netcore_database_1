ea48f89e8961eff5dd0bb9eeed1bf15ba4a153c5
askeip <paparazzi730@gmail.com>
2017-12-15 17:09:44 +0500

    init commit

---
+++ Extreme_DI/Services/StudentContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Extreme_DI.Models
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<StudentModel> Students { get; set; }
+
+        public StudentContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentModel>().HasKey(x => x.StudentID); ;
+        }
+    }
+}
